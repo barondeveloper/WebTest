f71c933a0fd4ef08c864ae1fbd11e0be392235c7
baron@thedevelopmentstore.com <baron@thedevelopmentstore.com>
2016-12-29 18:56:07 +0200

    now getting ip from outside

--- WebTest/JS/Test.js
+++ WebTest/JS/Test.js
@@ -8,12 +8,12 @@
     }
     // var link = 'http://xml.vudhaserve.com/search?feed=76743&auth=LdhZFu&subid=test&ua=Mozilla%2F5.0%20(Windows%20NT%206.1%3B%20WOW64%3B%20rv%3A13.0)%20Gecko%2F20100101%20Firefox%2F13.0.1&url=http%3A%2F%2Ftest.com%2F%3Fq%3Dbest%2Bdeals&user_ip=' + ip+'&query=best+deals';
     var settings = {
-        link:'http://xml.vudhaserve.com/search',
+        link: 'http://xml.vudhaserve.com/search',
         feed: "76743",
         uath: 'LdhZFu',
         ua: navigator.userAgent,// 'Mozilla%2F5.0%20(Windows%20NT%206.1%3B%20WOW64%3B%20rv%3A13.0)%20Gecko%2F20100101%20Firefox%2F13.0.1',
         url: 'http%3A%2F%2Ftest.com%2F%3Fq%3Dbest%2Bdeals',//location.href,//
-        user_ip: '185.120.124.62',
+        //user_ip: '185.120.124.62',
         query: 'best+deals'
     };
 
@@ -21,31 +21,35 @@
         $("#thediv").html("pixel= " + pixel + "<br/> url= " + url);
     });
 
-    function loadData  (settings, callback) {
+    function loadData(settings, callback) {
+
         setTimeout(function () {
-            var link = settings.link;
-            link = link + '?feed=' + settings.feed;
-            link = link + '&auth=' + settings.uath;
-            link = link + '&ua=' + settings.ua;
-            link = link + '&url=' + settings.url;
-            link = link + '&user_ip=' + settings.user_ip;
-            link = link + '&query=' + settings.query;
+            $.get('http://whatsmyip.thedevelopmentstore.com/api/translate', {}, function (resIp) {
 
-            $.get(link, {}, function (data) {
-                var nodes = data.documentElement.childNodes;
-                for (var i = 0; i < nodes.length; i++) {
-                    var node = nodes[i];
-                    if (node.localName === "listing") {
-                        var pixel = node.getAttribute("pixel");
-                        var url = node.getAttribute("url");
-                        callback(url, pixel);
+                settings.user_ip = resIp;
+
+                var link = settings.link;
+                link = link + '?feed=' + settings.feed;
+                link = link + '&auth=' + settings.uath;
+                link = link + '&ua=' + settings.ua;
+                link = link + '&url=' + settings.url;
+                link = link + '&user_ip=' + settings.user_ip;
+                link = link + '&query=' + settings.query;
+
+                $.get(link, {}, function (data) {
+                    var nodes = data.documentElement.childNodes;
+                    for (var i = 0; i < nodes.length; i++) {
+                        var node = nodes[i];
+                        if (node.localName === "listing") {
+                            var pixel = node.getAttribute("pixel");
+                            var url = node.getAttribute("url");
+                            callback(url, pixel);
+                        }
                     }
-                }
+                });
+
             });
-        }, 700);
+        }, 500);
     }
-
-
-
 } ())
 
