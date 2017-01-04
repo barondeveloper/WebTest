5b6024d6e4e6d935b32f510d61cc4c238c02a79d
baron@thedevelopmentstore.com <baron@thedevelopmentstore.com>
2017-01-04 21:40:25 +0200

    added jsdocs

--- WebTest/JS/Test.js
+++ WebTest/JS/Test.js
@@ -7,28 +7,41 @@
         document.getElementsByTagName('head')[0].appendChild(script);
     }
 
-    var settings = { 
+    var settings = {
         ua: navigator.userAgent,// 'Mozilla%2F5.0%20(Windows%20NT%206.1%3B%20WOW64%3B%20rv%3A13.0)%20Gecko%2F20100101%20Firefox%2F13.0.1',
         url: 'http%3A%2F%2Ftest.com%2F%3Fq%3Dbest%2Bdeals',//location.href,//
-        //user_ip: '185.120.124.62',
-        subid:'test',
+        subid: 'test',
         query: 'best+deals'
     };
 
     loadData(settings, function (url, pixel) {
-        var height = screen.height * 0.9 + 'px';
-        var width = screen.width * 0.7 + 'px';
-        window.name = 'urlPopUp';
-        var popupWindow = new Object();
-        popupWindow.closed = true;
-        popupWindow = window.open('', '_blank', `toolbar=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=${width}px,height=${height}px`);
-        popupWindow.location = url;
-
+        openWindow(url);
         //$("#thediv").html("pixel= " + pixel + "<br/> url= " + url);
     });
 
-    function loadData(settings, callback) {
+    /**
+    *@param {string} url - the url of the window to open
+    */
+    function openWindow(url) {
+        try {
+            var height = screen.height * 0.9 + 'px';
+            var width = screen.width * 0.7 + 'px';
+            window.name = 'urlPopUp';
+            var popupWindow = new Object();
+            popupWindow.closed = true;
+            popupWindow = window.open('', '_blank', `toolbar=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=${width}px,height=${height}px`);
+            popupWindow.location = url;
+        }
+        catch (e) {
+            console.log('could not open popup');
+        }
+    }
 
+    /** @description loads the data from the server.  
+     * @param {settings} settings -the setting that tells us how to get the data
+     * @param {function} callback - what happens after we have the data
+     */
+    function loadData(settings, callback) {
         setTimeout(function () {
             var fromUrl = 'http://whatsmyip.thedevelopmentstore.com/translate'
             $.post(fromUrl, settings, function (result) {
